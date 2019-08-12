46bd3ddc830dbbebe01a9a85231d72ec1a8316c6
Mlungisi <mgmgwala@gmail.com>
2019-08-12 16:40:30 +0200

    unit tests

---
+++ Jasmine/spec/AddSpec.js
@@ -0,0 +1,11 @@
+describe("Add", function(){
+	it("should return 0 when adding 0 + 0.",function(){
+		expect(add(0,0)).toEqual(0);
+	});
+	it("should return -2 when adding -1 + -1",function(){
+		expect(add(-1,-1)).toEqual(-2);
+	});
+	it("should return 9 when adding 4 + 5",function(){
+		expect(add(4,5)).toEqual(9);
+	});
+});
