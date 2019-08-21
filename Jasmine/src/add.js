ae08df233cfddbe6610d468cdf8d0adb7e907127
Mlungisi <mgmgwala@gmail.com>
2019-08-21 10:45:31 +0200

    simplified functions add.js and multiply.js

--- Jasmine/src/add.js
+++ Jasmine/src/add.js
@@ -1,19 +1,9 @@
 function add()
 {
-	var arr = [];
 	var i = 0;
 	var sum = 0;
 
 	while (i < arguments.length)
-	{
-		arr[i] = arguments[i++];
-	}
-	i = 0;
-	while (i < arr.length)
-	{
-		var num = arr[i++];
-		num = Number(num);
-		sum = sum + num;
-	}
+		sum = sum + arguments[i++];
 	return (sum);
 }
--- Jasmine/src/multiply.js
+++ Jasmine/src/multiply.js
@@ -1,17 +1,10 @@
 function multiply()
 {
-	var arr = [];
 	var i = 0;
 	var multi = 1;
 	
 	while (i < arguments.length)
-		arr[i] = arguments[i++];
-	i = 0;
-	while (i < arr.length)
-	{
-		var num = arr[i++];
-		num = Number(num);
-		multi = multi * num;
-	}
+		multi = multi * arguments[i++];
+
 	return (multi);
 }
